a7519cd21300cb6e26b9279f7d3f3a1f91063569
João Pedro Felicio Pereira <72157587+joaocabeca2@users.noreply.github.com>
2022-12-14 20:16:20 -0300

    Atualizando sintaxe do próprio selenium
    Boa tarde amigo. Sou leigo, mas acho que seu código estava um pouco desatualizado devida as novas releases do selenium quanto ao nome de algumas funções. Além disso, adicionei um parâmetro em "_get_names", pois ao inspecionar a pagina do instagram o XPATH era diferente de seguidores para seguindo em apenas um caractere.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from time import sleep
 from secrets import pw
 
@@ -11,40 +12,44 @@ class Instabot:
         sleep(2)
 
         # Digita Login
-        self.driver.find_element_by_xpath('//input[@name=\"username\"]')\
+        self.driver.find_element(By.XPATH,'//input[@name=\"username\"]')\
             .send_keys(username)
         # Digita Senha
-        self.driver.find_element_by_xpath('//input[@name=\"password\"]')\
+        self.driver.find_element(By.XPATH,'//input[@name=\"password\"]')\
             .send_keys(pw)
         # Clica login
-        self.driver.find_element_by_xpath('//button[@type="submit"]')\
+        self.driver.find_element(By.XPATH,'//button[@type="submit"]')\
             .click()
-
+        
         sleep(4)
-        self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
+        self.driver.find_element(By.XPATH,"//button[contains(text(), 'Agora não')]")\
             .click()
 
         sleep(2)
-        self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
+        self.driver.find_element(By.XPATH,"//button[contains(text(), 'Agora não')]")\
             .click()
         sleep(2)
 
     def get_unfollowers(self):
-        self.driver.find_element_by_xpath("//a[contains(@href, '/{}/')]".format(self.username))\
+        id_following = 3
+        id_followers = 2
+        self.driver.find_element(By.XPATH,"//a[contains(@href, '/{}/')]".format(self.username))\
             .click()
         sleep(2)
-        self.driver.find_element_by_xpath("//a[contains(@href, '/following')]")\
+        self.driver.find_element(By.XPATH,"//a[contains(@href, '/{}/following')]".format(self.username))\
             .click()
-        following = self._get_names()
-        self.driver.find_element_by_xpath("//a[contains(@href, '/followers')]")\
+        following = self._get_names(id_following)
+        self.driver.find_element(By.XPATH,"//a[contains(@href, '/{}/followers')]".format(self.username))\
             .click()
-        followers = self._get_names()
+        followers = self._get_names(id_followers)
         not_following_back = [user for user in following if user not in followers]
-        print(not_following_back)
 
-    def _get_names(self):
+        for name in not_following_back:
+            print(name)
+
+    def _get_names(self,id):
         sleep(5)
-        scroll_box = self.driver.find_element_by_xpath("/html/body/div[5]/div/div/div[3]")
+        scroll_box = self.driver.find_element(By.XPATH,"/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[{}]".format(id))
         last_ht, ht = 0 , 1
 
         while last_ht != ht:
@@ -54,15 +59,15 @@ class Instabot:
                 arguments[0].scrollTo(0, arguments[0].scrollHeight);
                 return arguments[0].scrollHeight
             """, scroll_box)
-        links = scroll_box.find_elements_by_tag_name('a')
+        links = scroll_box.find_elements(By.TAG_NAME,'a')
         names = [name.text for name in links if name != '']
 
         #Close Button
-        self.driver.find_element_by_xpath("/html/body/div[5]/div/div/div[1]/div/div[2]/button")\
+        self.driver.find_element(By.XPATH,"/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/button")\
             .click()
         return names
 
         
 
-meuBot = Instabot('eduardo_reinheimer', pw)
+meuBot = Instabot('username', pw)
 meuBot.get_unfollowers()
